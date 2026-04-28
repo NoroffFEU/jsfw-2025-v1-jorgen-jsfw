e3bd82857cebfeb7d19a92cd2cc41a348c9ea5cf
jb12-art <jbqw1@msn.com>
2026-04-28 12:04:29 +0300

    header tsx and css

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+// src/components.Header.tsx
+
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Online Shop</h1>
+    </header>
+  );
+}
