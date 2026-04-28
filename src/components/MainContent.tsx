635f38368884377c8ef9ead7d1fa09c11d796e7e
jb12-art <jbqw1@msn.com>
2026-04-28 15:47:09 +0300

    ReactNode

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,16 @@
 // src/components/MainContent.tsx
 
+import type { ReactNode } from 'react'; // Import ReactNode for TypeScript
 import styles from './MainContent.module.css';
 
-export default function MainContent() {
+interface Props {
+  children: ReactNode; // This tells TS that MainContent can wrap other elements
+}
+
+export default function MainContent({ children }: Props) {
   return (
     <main className={styles.mainContainer}>
-      <h2>Shop fast and easy</h2>
+      {children} {/* where the Searchbar will appear */}
     </main>
   );
 }
