97a9ee851e405f18d1e2a2756917b79f30aab68c
jb12-art <jbqw1@msn.com>
2026-04-28 12:06:27 +0300

    the rest of the project setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@
 // import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main from './components/Main';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <MainContent />
       <Footer />
     </>
   );
