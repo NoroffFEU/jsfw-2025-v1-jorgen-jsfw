d5ed7eb0b24db961f9a501ef775fdd2b1faee890
jb12-art <jbqw1@msn.com>
2026-04-28 12:05:41 +0300

    MainContent tsx and css

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,11 @@
+// src/components/MainContent.tsx
+
+import styles from './MainContent.module.css';
+
+export default function MainContent() {
+  return (
+    <main className={styles.mainContainer}>
+      <h2>Shop fast and easy</h2>
+    </main>
+  );
+}
