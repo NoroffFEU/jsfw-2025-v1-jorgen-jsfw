b46f2d3cded94ae4dfe0cc4cf537e4562c8c032f
jb12-art <jbqw1@msn.com>
2026-05-04 17:02:55 +0300

    import rating, Price, getDiscount function, styling of the product card info.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,57 +3,40 @@
 import { Link } from 'react-router-dom';
 import type { Product } from '../types/product';
 import styles from './ProductCard.module.css';
+import Rating from '../components/Rating';
+import Price from './Price';
+import { getDiscount } from '../utils/price';
 
 interface Props {
   product: Product;
 }
 
 export default function ProductCard({ product }: Props) {
-  // check if product has discount
-  const hasDiscount =
-    product.discountedPrice && product.discountedPrice < product.price;
-
-  // calculate discount %
-  const discountPercent = hasDiscount
-    ? Math.round(
-        ((product.price - product.discountedPrice!) / product.price) * 100,
-      )
-    : 0;
+  const { hasDiscount, discountPercent } = getDiscount(
+    product.price,
+    product.discountedPrice,
+  );
 
   return (
     <Link to={`/product/${product.id}`} className={styles.card}>
+      {/* discount badge logic is in src/components/Price.tsx */}
+      {hasDiscount && <span className={styles.badge}>-{discountPercent}%</span>}
+
       {/* image */}
       <img
         src={product.image?.url || '/placeholder.jpg'}
         alt={product.image?.alt || product.title}
         className={styles.img}
       />
-      {/* discount badge */}
-      {hasDiscount && <span className={styles.badge}>-{discountPercent}%</span>}
+
       {/* title */}
       <h3>{product.title}</h3>
-      {/* price */}
-      <p>
-        {hasDiscount ? (
-          <>
-            <span className={styles.oldPrice}>{product.price} kr</span>
-            {''}
-            <strong>{product.discountedPrice} kr</strong>
-          </>
-        ) : (
-          <strong>{product.price} kr</strong>
-        )}
-      </p>
-      {/* rating */}
-      <div className={styles.ratingContainer}>
-        <span className={styles.starIcon}>{'\u2605'}</span>
-        <span className={styles.ratingText}>
-          {product.rating % 1 === 0
-            ? product.rating
-            : product.rating.toFixed(1)}{' '}
-          / 5
-        </span>
-      </div>
+
+      {/* price logic is in src/components/Price.tsx  */}
+      <Price price={product.price} discountedPrice={product.discountedPrice} />
+
+      {/* rating logic is in src/components/Rating.tsx */}
+      <Rating rating={product.rating} />
     </Link>
   );
 }
