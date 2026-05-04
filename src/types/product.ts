66b56172499007383b3e3f234c5ac6a00f1c2b0b
jb12-art <jbqw1@msn.com>
2026-05-04 09:33:32 +0300

    export interface Product,
    export interface Review

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,1 +1,23 @@
 // src/types/product.ts
+
+export interface Product {
+  id: string;
+  title: string;
+  image: {
+    url: string;
+    alt?: string;
+  };
+  price: number;
+  description: string;
+  tags?: string[];
+  discountedPrice?: number;
+  rating: number;
+  reviews?: Review[];
+}
+
+export interface Review {
+  id: string;
+  username: string;
+  rating: number;
+  description: string;
+}
