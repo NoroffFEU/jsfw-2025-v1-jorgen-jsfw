bc06cd118bc143da1148235e9f3b03a688b527d5
jb12-art <jbqw1@msn.com>
2026-05-05 11:44:55 +0300

    magnifying glass icon on searchbar, styles

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -4,6 +4,13 @@ import styles from './Searchbar.module.css';
 
 export default function Searchbar() {
   return (
-    <input className={styles.searchbar} placeholder="Search for items"></input>
+    <div className={styles.container}>
+      <span className={styles.icon}>{'\u2315'}</span>
+      <input
+        type="text"
+        className={styles.searchbar}
+        placeholder="Search for items"
+      />
+    </div>
   );
 }
