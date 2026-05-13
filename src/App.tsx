c24b4c5c043158932a27f5d95196361866ecd25f
jb12-art <jbqw1@msn.com>
2026-05-13 08:33:25 +0300

    add: include CartPage and SuccessPage routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import ContactPage from './pages/ContactPage';
+import CartPage from './pages/CartPage';
+import SuccessPage from './pages/SuccessPage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/success" element={<SuccessPage />} />
         </Routes>
       </BrowserRouter>
     </>
